35100c7d3e292f5aeeef9f3218eba8965a970c77
haykam821 <haykam821@users.noreply.github.com>
2018-06-02 12:17:06 -0400

    Allow using middle coordinates

--- index.js
+++ index.js
@@ -34,28 +34,33 @@ function drawThing(item, x = fallbackPos, y = fallbackPos) {
 	ctx.drawImage(images[item.url], x, y);
 }
 
-function calc(pos, height) {
+function calc(pos, item, useMiddle, height) {
 	const size = height ? can.height : can.width;
+	const imgSize = height ? images[item.url].height : images[item.url].width;
+
+	let position = fallbackPos;
 
 	if (pos.toString().endsWith("%")) {
 		const percent = parseInt(pos.slice(0, -1));
 		if (isNaN(percent)) {
-			return fallbackPos;
+			return position;
 		} else {
-			return percent / 100 * size;
+			position = percent / 100 * size;
 		}
 	} else {
 		const newPos = parseInt(pos);
 		if (isNaN(newPos)) {
-			return fallbackPos;
+			return position;
 		} else {
-			return newPos;
+			position = newPos;
 		}
 	}
+
+	return position - (useMiddle ? imgSize / 2 : 0);;
 }
 
 function drawAll() {
-	json = JSON.stringify(JSON.parse(input.value), undefined, "\t");
+	const json = JSON.stringify(JSON.parse(input.value), undefined, "\t");
 
 	input.value = json;
 	localStorage.savedJSON = json;
@@ -63,15 +68,15 @@ function drawAll() {
 	ctx.clearRect(0, 0, can.width, can.height);
 
 	for (const item of JSON.parse(input.value).reverse()) {
-		if (!images[item.url]) {
+		if (images[item.url]) {
+			drawThing(item, calc(item.x, item), calc(item.y, item, true));
+		} else {
 			images[item.url] = new Image();
 			images[item.url].src = item.url;
 
 			images[item.url].onload = function() {
-				drawThing(item, calc(item.x), calc(item.y, true));
+				drawThing(item, calc(item.x, item, item.fromCenter), calc(item.y, item, item.fromCenter, true));
 			};
-		} else {
-			drawThing(item, calc(item.x), calc(item.y, true));
 		}
 	}
 }
